2cbbd46ea1648a10bf0a6f143b497f5367165df2
AK47 <myfavoredcolorisblue@gmail.com>
2015-01-21 21:47:55 +0200

    Mouse wheel slow down

--- Source/CoeusList.cpp
+++ Source/CoeusList.cpp
@@ -608,7 +608,7 @@ void CoeusList::mouseExit(const MouseEvent &event)
 void CoeusList::mouseWheelMove (const MouseEvent &event, const MouseWheelDetails &wheel)
 {
     const double cStart = sb.getCurrentRangeStart();
-    const double nCStart = cStart + wheel.deltaY;
+    const double nCStart = cStart + wheel.deltaY*0.1;
     sb.setCurrentRangeStart(nCStart);
 }
 
